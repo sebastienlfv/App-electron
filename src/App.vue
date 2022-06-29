52497480dda0f866641813d8cdbacd83c54832cf
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-06-29 13:17:34 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/" class="profile">
+      <img src="./assets/frz.png" alt="">
+      <p>FRZ</p>
+    </router-link>
+    <div class="navigation">
+      <router-link to="/">Home</router-link>
+      <router-link to="/">Server</router-link>
+      <router-link to="/">VIP</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-link to="/">Sign Up</router-link>
   </nav>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
+// style css
+import ('./style/app.scss')
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
